c94dbdd28704b3218a59996360d93e1ab6c20a2b
Matthew Voss <mavoss16@gmail.com>
2024-01-16 21:56:49 -0600

    Delete extra shiny_data

---
+++ scripts/Daily/daily_run.R
@@ -0,0 +1,8 @@
+
+
+source("C:/Users/mavos/Documents/GitHub/iowa_legislature/scripts/Daily/daily_01_read_file_actions.R")
+source("C:/Users/mavos/Documents/GitHub/iowa_legislature/scripts/Daily/daily_02_scrape_vote_results.R")
+source("C:/Users/mavos/Documents/GitHub/iowa_legislature/scripts/Daily/daily_03_read_votes.R")
+source("C:/Users/mavos/Documents/GitHub/iowa_legislature/scripts/Daily/daily_04_legislation_clean.R")
+source("C:/Users/mavos/Documents/GitHub/iowa_legislature/scripts/Daily/daily_05_legislators_sponsor_fm_scrape.R")
+source("C:/Users/mavos/Documents/GitHub/iowa_legislature/scripts/Daily/daily_06_legislators_clean.R")
